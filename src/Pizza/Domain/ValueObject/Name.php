76dcf64bccbed2600c1c5be725aeaee66de9efcc
Jcoca <jcoca@factorenergia.com>
2024-03-10 19:08:14 +0100

    agregamos -> validaciones para el nombre de la pizza

--- src/Pizza/Domain/ValueObject/Name.php
+++ src/Pizza/Domain/ValueObject/Name.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Pizza\Domain\ValueObject;
 
 use App\Common\Domain\ValueObject\StringValueObject;
+use App\Pizza\Domain\Exception\InvalidPizzaName;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -19,11 +20,11 @@ class Name extends StringValueObject
     protected function saveIfIsAllowed(?string $value): void
     {
         if (in_array($value, ['', null], true)) {
-            // TODO THROW EXCEPTION
+            throw new InvalidPizzaName('Name is required');
         }
 
         if (strlen($value) > self::LENGTH_MAX) {
-            // TODO THROW EXCEPTION
+            throw new InvalidPizzaName(sprintf('%s is greater than %s characters', $value, self::LENGTH_MAX));
         }
     }
 }
