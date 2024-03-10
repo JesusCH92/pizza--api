3482b0e771fbb241403f7596076cb46f41f4f9b6
Jcoca <jcoca@factorenergia.com>
2024-03-10 22:57:14 +0100

    configuramos -> mapeo en json de los errores

---
+++ src/Common/Infrastructure/Framework/ErrorListener.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Common\Infrastructure\Framework;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+
+class ErrorListener
+{
+    public function __invoke(ExceptionEvent $event): void
+    {
+        // You get the exception object from the received event
+        $exception = $event->getThrowable();
+        $message = sprintf(
+            'My Error says: %s with code: %s',
+            $exception->getMessage(),
+            $exception->getCode()
+        );
+
+        // Customize your response object to display the exception details
+        $response = new JsonResponse();
+        $response->setData(['ok' => false, 'error' => ['message' => $message,]]);
+        $response->headers->set('Content-Type', 'application/json');
+
+        // HttpExceptionInterface is a special type of exception that
+        // holds status code and header details
+        if ($exception instanceof HttpExceptionInterface) {
+            $response->setStatusCode($exception->getStatusCode());
+        } else {
+            $response->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        // sends the modified response object to the event
+        $event->setResponse($response);
+    }
+}
