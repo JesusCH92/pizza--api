628357d58d349c00e77abfa1ada8b09e55e4d547
Jcoca <jcoca@factorenergia.com>
2024-03-10 18:19:54 +0100

    implementamos -> puerto para pizza

---
+++ src/Pizza/Domain/Repository/PizzaRepository.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Pizza\Domain\Repository;
+
+use App\Pizza\Domain\Entity\Pizza;
+
+interface PizzaRepository
+{
+    public function save(Pizza $pizza): void;
+}
