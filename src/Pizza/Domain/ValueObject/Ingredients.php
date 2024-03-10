56e8069e8c8a97172523e104e2235381f1c291fa
Jcoca <jcoca@factorenergia.com>
2024-03-10 18:03:25 +0100

    implementamos -> VO tipo array para los ingredientes

---
+++ src/Pizza/Domain/ValueObject/Ingredients.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Pizza\Domain\ValueObject;
+
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Embeddable]
+class Ingredients
+{
+    const AMOUNT_MAX = 20;
+    #[ORM\Column(name: 'ingredients', type: Types::JSON, nullable: false)]
+    private array $ingredients;
+
+    public function __construct(string ...$ingredients)
+    {
+        $this->setIngredients(...$ingredients);
+    }
+
+    private function setIngredients(string ...$ingredients): void
+    {
+        $this->saveIfIsAllowed(...$ingredients);
+        $this->ingredients = array_unique($ingredients);
+    }
+
+    private function saveIfIsAllowed(string ...$ingredients): void
+    {
+        $amount = count($ingredients);
+
+        if ($amount > self::AMOUNT_MAX) {
+            // TODO THROW EXCEPTION
+        }
+    }
+
+    public function value(): array
+    {
+        return $this->ingredients;
+    }
+}
