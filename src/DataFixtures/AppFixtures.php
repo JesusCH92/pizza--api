bbd6fd54b7fa1595038549b569e897d21960f6b3
Jcoca <jcoca@factorenergia.com>
2024-03-10 22:44:40 +0100

    instalamos -> carga de pizzas de prueba

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,29 @@
 
 namespace App\DataFixtures;
 
+use App\Pizza\ApplicationService\DTO\PizzaMakerRequest;
+use App\Pizza\ApplicationService\PizzaMaker;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
+    public function __construct(private readonly PizzaMaker $pizzaMaker)
+    {
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        for($i = 0; $i < 20; $i++) {
+            ($this->pizzaMaker)(
+                new PizzaMakerRequest(
+                    'pizza_' . $i,
+                    null,
+                    false,
+                    ['ingredient_1', 'ingredient_2']
+                )
+            );
+        }
 
         $manager->flush();
     }
